3bd3407d50f66aecff76a266d67721d8e83973e5
minhkhoi <minhkhoi.pham@livingobjects.com>
2014-12-30 18:23:02 +0100

    add rookcheck

--- src/model/piece/check/RookCheck.java
+++ src/model/piece/check/RookCheck.java
@@ -1,6 +1,7 @@
 package model.piece.check;
 
 import model.piece.Rook;
+import model.plateform.Square;
 import model.plateform.Table;
 
 public class RookCheck implements ICheck {
@@ -12,6 +13,67 @@ public class RookCheck implements ICheck {
 
 	@Override
 	public void check(Table table) {
-
+		int row = this.rook.getPosition().getRowNumber();
+		int column = this.rook.getPosition().getColumnNumber();
+		boolean next = true;
+		int i = 1;
+		while (next) {
+			if (column - i > 0) {
+				Square square = table.getSquare(row, column - i);
+				square.getCheckedBy().add(this.rook);
+				if (!square.isOccupied()) {
+					i++;
+				} else {
+					next = false;
+				}
+			} else {
+				next = false;
+			}
+		}
+		next = true;
+		i = 1;
+		while (next) {
+			if (column + i <= 8) {
+				Square square = table.getSquare(row, column + i);
+				square.getCheckedBy().add(this.rook);
+				if (!square.isOccupied()) {
+					i++;
+				} else {
+					next = false;
+				}
+			} else {
+				next = false;
+			}
+		}
+		next = true;
+		i = 1;
+		while (next) {
+			if (row - i > 0) {
+				Square square = table.getSquare(row - i, column);
+				square.getCheckedBy().add(this.rook);
+				if (!square.isOccupied()) {
+					i++;
+				} else {
+					next = false;
+				}
+			} else {
+				next = false;
+			}
+		}
+		next = true;
+		i = 1;
+		while (next) {
+			if (row + i <= 8) {
+				Square square = table.getSquare(row + i, column);
+				square.getCheckedBy().add(this.rook);
+				if (!square.isOccupied()) {
+					i++;
+				} else {
+					next = false;
+				}
+			} else {
+				next = false;
+			}
+		}
 	}
 }
